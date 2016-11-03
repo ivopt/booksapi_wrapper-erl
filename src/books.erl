586118e13c72b0b5f30b95e33bd229ed79dde26f
Ivo Jesus <ijesus@equalexperts.com>
2016-11-03 16:51:48 +0000

    Merged the books app and books supervisor

--- src/books.erl
+++ src/books.erl
@@ -1,13 +1,19 @@
 -module(books).
 -behaviour(application).
+-behaviour(supervisor).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % OTP application behaviour API
 -export([start/2, stop/1]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% OTP supervisor behaviour API
+-export([start_link/0]).
+-export([init/1]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Client application API
--export([start/0, stop/0, search/1]).
+-export([start/0, stop/0, run_cli/1, search/1]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Escriptize main function
@@ -16,14 +22,29 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % OTP application behaviour API implementation
-start(_Type, _Args) -> books_sup:start_link().
+start(_Type, _Args) -> start_link().
 stop(_) -> ok.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% OTP supervisor behaviour API implementation
+start_link() -> supervisor:start_link(?MODULE, []).
+
+init(_) ->
+  {ok, {
+    {one_for_one, 1, 5},
+    [
+      #{ id    => books_repository_id,
+         start => {books_repository, start, ["https://www.googleapis.com/books/v1/volumes?q="]} }
+    ]
+  }}.
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Client application API implementation
 start() -> application:start(books).
 stop()  -> application:stop(books).
 
+run_cli(Args)   -> books_cli:run(Args, {?MODULE}).
 search(Options) -> books_repository:search_books(Options).
 
 
@@ -31,6 +52,6 @@ search(Options) -> books_repository:search_books(Options).
 % Escriptize main function implementation
 main(Args) ->
   start(),
-  Response = books_cli:run(Args, {?MODULE}),
+  Response = run_cli(Args),
   io:format(Response),
   stop().
